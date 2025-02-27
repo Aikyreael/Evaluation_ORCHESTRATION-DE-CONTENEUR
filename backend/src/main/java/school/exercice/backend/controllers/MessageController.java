037584494cf878cdd4059d6436bbd92a969d7cbe
DELL1 <aikyreael@protonmail.com>
2025-02-27 13:50:30 +0100

    Backend

--- backend/src/main/java/school/exercice/backend/controllers/MessageController.java
+++ backend/src/main/java/school/exercice/backend/controllers/MessageController.java
@@ -3,10 +3,7 @@ package school.exercice.backend.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.exercice.backend.entities.Message;
 import school.exercice.backend.services.MessageService;
 
@@ -25,7 +22,7 @@ public class MessageController {
     }
 
     @PostMapping("/messages")
-    public void saveMessage(@RequestBody Message message) {
+    public Message saveMessage(@RequestBody Message message) {
 
         Message savedMessage = messageService.saveMessage(message);
 
@@ -34,6 +31,8 @@ public class MessageController {
                 savedMessage.getDate(),
                 savedMessage.getContent()
         );
+
+        return savedMessage;
     }
 
     @GetMapping("/messages")
